c5be6fbc93401ee379f7be8bc148a225b48a9fbb
catalinm00 <catalin.marian123@gmail.com>
2024-11-21 17:46:59 +0100

    add env files

--- worker/src/app.module.ts
+++ worker/src/app.module.ts
@@ -1,31 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { ActivateFunctionListener } from './messaging/listener/ActivateFunctionListener';
-import { ExecuteFunctionService } from './service/ExecuteFunctionService';
-import { DockerConfig } from './config/DockerConfig';
 import { NatsService } from './service/NatsService';
 import * as process from 'node:process';
+import { DockerClient } from './config/docker';
+import { ExecuteFunctionServiceAlt } from './service/execute-function-service-alt';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: `../.env.${process.env.NODE_ENV || 'dev'}`,
+      envFilePath: `.env.${process.env.NODE_ENV || 'dev'}`,
     }),
-    ClientsModule.register([
-      {
-        name: 'NATS_CLIENT',
-        transport: Transport.NATS,
-        options: {
-          servers: [process.env.NATS_SERVER || 'nats://localhost:4222'],
-        },
+    ClientsModule.register([{
+      name: 'NATS_CLIENT',
+      transport: Transport.NATS,
+      options: {
+        servers: [process.env.NATS_SERVER || 'nats://localhost:4222'],
       },
-    ]),
+    }]),
   ],
-  controllers: [AppController, ActivateFunctionListener],
-  providers: [AppService, DockerConfig, ExecuteFunctionService, NatsService],
+  controllers: [ ActivateFunctionListener, AppController],
+  providers: [NatsService, DockerClient, ExecuteFunctionServiceAlt],
 })
-export class AppModule {}
+export class AppModule {
+}
