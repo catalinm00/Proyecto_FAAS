76ec4b8046168cc1e40a4150e8ff3c6f4d2f6a96
catalinm00 <catalin.marian123@gmail.com>
2024-12-27 18:55:00 +0100

    fix salting

--- api-server/src/user/infrastructure/config/criptography/bcrypt-service.ts
+++ api-server/src/user/infrastructure/config/criptography/bcrypt-service.ts
@@ -10,8 +10,8 @@ export class BcryptService implements CryptographyService {
 
   async encrypt(data: string): Promise<string> {
     let saltingWord = this.configService.get('SALTING_WORD');
-    const salt = await bcrypt.genSalt(this.iterations, saltingWord);
-    return bcrypt.hash(data, salt);
+    const salt = await bcrypt.genSalt(this.iterations, 'a');
+    return bcrypt.hash(data+saltingWord, salt);
   }
 
 }
