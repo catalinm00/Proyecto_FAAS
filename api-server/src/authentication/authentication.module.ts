669a7d79ef69da5399d8bde317c3cce9c8bc3f05
Isabell Helling <isabell.helling@gmail.com>
2025-01-10 15:23:51 +0100

    start implementation for adapting the endpoints to receive jwt tokens by creating jwt.strategy.ts and adapting authentication-module.ts TODO: updating the  functionController to use the JWT strategy

--- api-server/src/authentication/authentication.module.ts
+++ api-server/src/authentication/authentication.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { ApisixService } from './apisix.service';
+import { JwtStrategy } from './jwt.strategy';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-    imports: [JwtModule.register({})],
-    providers: [AuthService,ApisixService],
-    exports: [AuthService],
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: 'your_jwt_secret_key', // TODO: Replace with own secret key
+      signOptions: { expiresIn: '60m' },
+    }),
+  ],
+  providers: [AuthService, ApisixService, JwtStrategy],
+  exports: [AuthService],
 })
-export class AuthenticationModule{}
+export class AuthenticationModule {}
