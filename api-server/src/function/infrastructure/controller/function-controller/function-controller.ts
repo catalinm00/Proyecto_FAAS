dc7901b67a1e6e0d0c64c478595dde2460dd531d
Isabell Helling <isabell.helling@gmail.com>
2025-01-10 14:40:21 +0100

    add getFunctionById to FunctionController

--- api-server/src/function/infrastructure/controller/function-controller/function-controller.ts
+++ api-server/src/function/infrastructure/controller/function-controller/function-controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Logger, Post, Delete } from '@nestjs/common';
+import { Body, Controller, Logger, Post, Delete, Get } from '@nestjs/common';
 import { ApiResponse } from '@nestjs/swagger';
 import { CreateFunctionUseCase } from 'src/function/application/use-case/create-function-use-case';
 import { CreateFunctionRequest } from '../request/create-function-request';
@@ -6,6 +6,9 @@ import { CreateFunctionCommand } from 'src/function/application/command/create-f
 import { DeleteFunctionCommand } from 'src/function/application/command/delete-function-command';
 import { DeleteFunctionRequest } from '../request/delete-function-request';
 import { DeleteFunctionUseCase } from 'src/function/application/use-case/delete-function-use-case';
+import { GetFunctionByIdRequest } from '../request/get-function-by-id-request';
+import { GetFunctionByIdCommand } from 'src/function/application/command/get-function-by-id-command';
+import { GetFunctionByIdUseCase } from 'src/function/application/use-case/get-function-by-id-use-case';
 
 @Controller('api/v1/functions')
 export class FunctionController {
@@ -14,6 +17,7 @@ export class FunctionController {
   constructor(
     private readonly createFunctionService: CreateFunctionUseCase,
     private readonly deleteFunctionService: DeleteFunctionUseCase,
+    private readonly getFunctionByIdService: GetFunctionByIdUseCase,
   ) {}
 
   @Post()
@@ -53,6 +57,27 @@ export class FunctionController {
 
     return {
       message: 'Function deleted successfully.',
+    };
+  }
+
+  @Get() ///functions/{id}
+  @ApiResponse({
+    status: 201,
+    description: 'Function returned successfully.',
+  })
+  async getFunctionById(@Body() request: GetFunctionByIdRequest) {
+    this.logger.log(
+      `Returning function with ID: ${request.functionId} for user: ${request.userId}`,
+    );
+
+    const command = new GetFunctionByIdCommand(
+      request.functionId,
+      request.userId,
+    );
+    await this.getFunctionByIdService.execute(command);
+
+    return {
+      message: 'Function returned successfully.',
     };
   }
 }
