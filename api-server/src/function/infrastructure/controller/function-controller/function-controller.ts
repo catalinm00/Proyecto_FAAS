8e6f3bf822158694f39d03baecec383a007c48fe
Isabell Helling <isabell.helling@gmail.com>
2025-01-10 21:03:48 +0100

    updated the FunctionController to use the JWT strategy

--- api-server/src/function/infrastructure/controller/function-controller/function-controller.ts
+++ api-server/src/function/infrastructure/controller/function-controller/function-controller.ts
@@ -1,5 +1,12 @@
-import { Body, Controller, Logger, Post, Delete } from '@nestjs/common';
-import { ApiResponse } from '@nestjs/swagger';
+import {
+  Body,
+  Controller,
+  Logger,
+  Post,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
+import { ApiBearerAuth, ApiResponse } from '@nestjs/swagger';
 import { CreateFunctionUseCase } from 'src/function/application/use-case/create-function-use-case';
 import { CreateFunctionRequest } from '../request/create-function-request';
 import { CreateFunctionCommand } from 'src/function/application/command/create-function-command';
@@ -9,6 +16,7 @@ import { DeleteFunctionUseCase } from 'src/function/application/use-case/delete-
 import { ExecuteFunctionRequest } from '../request/execute-function-request';
 import { ExecuteFunctionUseCase } from '../../../application/use-case/execute-function-usecase';
 import { ExecuteFunctionCommand } from '../../../application/command/execute-function-command';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('api/v1/functions')
 export class FunctionController {
@@ -21,6 +29,8 @@ export class FunctionController {
   ) {}
 
   @Post()
+  @UseGuards(AuthGuard('jwt'))
+  @ApiBearerAuth()
   @ApiResponse({
     status: 201,
     description: 'Function created successfully.',
@@ -40,6 +50,8 @@ export class FunctionController {
   }
 
   @Delete() ///functions/{id}
+  @UseGuards(AuthGuard('jwt'))
+  @ApiBearerAuth()
   @ApiResponse({
     status: 201,
     description: 'Function deleted successfully.',
@@ -61,6 +73,8 @@ export class FunctionController {
   }
 
   @Post('/execute')
+  @UseGuards(AuthGuard('jwt'))
+  @ApiBearerAuth()
   async executeFunction(@Body() request: ExecuteFunctionRequest) {
     const command: ExecuteFunctionCommand = new ExecuteFunctionCommand(
       request.functionId,
