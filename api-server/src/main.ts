ba78448bd49baafef024233da1df7ed7e185ce6e
catalinm00 <catalin.marian123@gmail.com>
2025-01-11 15:55:44 +0100

    fix swagger access

--- api-server/src/main.ts
+++ api-server/src/main.ts
@@ -4,23 +4,15 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // Configuración CORS específica
-  app.enableCors({
-    origin: 'http://localhost:3000',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-API-KEY'],
-    credentials: true,
-  });
 
   const config = new DocumentBuilder()
     .setTitle('API-SERVER')
     .setDescription('API used for the management of users and functions')
     .setVersion('1.0')
-    .addServer('http://localhost:9080/', 'APISIX Gateway')
     .addBearerAuth()
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
+  SwaggerModule.setup('api-docs', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
 }
