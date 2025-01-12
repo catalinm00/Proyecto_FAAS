5ad04f3fd4c7be0007fc1cbd1aec88ea73aa1e2a
catalinm00 <catalin.marian123@gmail.com>
2025-01-12 18:44:17 +0100

    updated token expiration to match expiration cobnfigured in apisix

--- api-server/src/authentication/authentication.module.ts
+++ api-server/src/authentication/authentication.module.ts
@@ -11,7 +11,7 @@ import { JwtService } from './jwt.service';
     PassportModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '60m' },
+      signOptions: { expiresIn: '24h' },
     }),
   ],
   providers: [AuthService, ApisixService, JwtStrategy, JwtService],
