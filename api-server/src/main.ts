d2ad2ed14d9be246577fd42575b3524b4bd42089
catalinm00 <catalin.marian123@gmail.com>
2025-01-17 23:22:23 +0100

    api-server: replaced default logger with pino for better performance

--- api-server/src/main.ts
+++ api-server/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { logger: ["debug"], bufferLogs: true });
+  app.useLogger(app.get(Logger));
 
   const config = new DocumentBuilder()
     .setTitle('API-SERVER')
